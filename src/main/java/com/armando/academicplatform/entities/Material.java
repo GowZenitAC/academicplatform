86b24b0f2ea1ee3d681676fef18a21a3a4f0a545
GowZenitAC <jes_alpuche@outlook.es>
2025-06-04 10:35:56 -0600

    material entitie is finished

--- src/main/java/com/armando/academicplatform/entities/Material.java
+++ src/main/java/com/armando/academicplatform/entities/Material.java
@@ -1,7 +1,6 @@
 package com.armando.academicplatform.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +11,23 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Material {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, length = 500)
+    private String file_url;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    @ManyToOne(fetch = FetchType.LAZY)  @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher teacher;
+
 }
