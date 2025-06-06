a58339448bc07da7879eb5301f32221912dfd721
GowZenitAC <jes_alpuche@outlook.es>
2025-06-05 19:19:31 -0600

    Update SpringSecurityConfig.java

--- src/main/java/com/armando/academicplatform/security/SpringSecurityConfig.java
+++ src/main/java/com/armando/academicplatform/security/SpringSecurityConfig.java
@@ -1,23 +1,39 @@
 package com.armando.academicplatform.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para APIs REST
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // permite TODO temporalmente
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll() // Permitir registro y login
+                        .anyRequest().authenticated() // Otros endpoints requieren autenticación
                 )
-                .sessionManagement(m -> m.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-                // .addFilter(new JwtValidationFilter(authenticationManager()))
+                .sessionManagement(m -> m.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin sesiones
                 .build();
     }
 
