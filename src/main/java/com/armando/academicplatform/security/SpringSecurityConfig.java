58c77ecff8828fb6b1a9adfdcc48f1ed58605981
GowZenitAC <jes_alpuche@outlook.es>
2025-06-10 10:26:10 -0600

    material module is ready

--- src/main/java/com/armando/academicplatform/security/SpringSecurityConfig.java
+++ src/main/java/com/armando/academicplatform/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import com.armando.academicplatform.security.filter.JwtValidationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,9 +37,17 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/register").permitAll() // Permitir registro y login
                         .requestMatchers("/api/students/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/subjects").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/api/courses").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/subjects").hasRole("TEACHER")
+                        .requestMatchers(HttpMethod.POST, "/api/notes").hasRole("TEACHER")
+                        .requestMatchers(HttpMethod.POST, "/api/materials").hasRole("TEACHER")
+                        .requestMatchers(HttpMethod.GET, "/api/notes/student/{studentId}").hasRole("STUDENT")
+                        .requestMatchers(HttpMethod.GET, "/api/notes/subject/{subjectId}").hasAnyRole("STUDENT", "TEACHER")
+                        .requestMatchers(HttpMethod.GET, "/api/materials/subjects/{id}").hasAnyRole("STUDENT")
                         .requestMatchers("/api/teachers/**").hasRole("ADMIN")
                         .requestMatchers("/api/periods/**").hasRole("ADMIN")
-                        .anyRequest().authenticated() // Otros endpoints requieren autenticación
+                        .anyRequest().permitAll() // Otros endpoints requieren autenticación
                 )
                 .sessionManagement(m -> m.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin sesiones
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
